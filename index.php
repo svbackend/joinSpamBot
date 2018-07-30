4f5c67a1fb1e49c7aafa952822ee42d7abe29886
Valentin <svbackend22@gmail.com>
2018-07-30 09:59:41 +0300

    Command to remove messages about join user with long name

--- index.php
+++ index.php
@@ -5,10 +5,15 @@ require __DIR__ . '/vendor/autoload.php';
 $bot_api_key  = \getenv('token');
 $bot_username = \getenv('name');
 
+$commands_paths = [
+    __DIR__ . '/Commands/',
+];
+
 try {
     // Create Telegram API object
     $telegram = new Longman\TelegramBot\Telegram($bot_api_key, $bot_username);
 
+    $telegram->addCommandsPaths($commands_paths);
     // Handle telegram webhook request
     $telegram->handle();
 } catch (Longman\TelegramBot\Exception\TelegramException $e) {
